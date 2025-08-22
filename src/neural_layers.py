5047aad6d13d8540a070a48ee5ce2bf4f6c1969a
hghrv <htchoumkeu@yahoo.fr>
2025-08-22 05:58:44 +0100

    Setting the one_hot encoding for the target values

--- src/neural_layers.py
+++ src/neural_layers.py
@@ -1,3 +1,4 @@
+# Importing Keras and other necessary libraries for the Python API of Tensorflow
 import numpy as np
 import pickle
 import gzip
@@ -22,7 +23,8 @@ np.random.seed(7)
 
 
 # %matplotlib inline
- 
+
+# Loading the training dataset
 def load_data():
     f = gzip.open('mnist.pkl.gz', 'rb')
     f.seek(0)
@@ -33,5 +35,21 @@ def load_data():
 training_data, validation_data, test_data = load_data()
 
 training_data
+
+# Setting the one_hot encoding
+def one_hot(j):
+    # input is the target dataset of shape (1, m) where m is the number of data points
+    # returns a 2 dimensional array of shape (10, m) where each target value is converted to a one hot encoding
+    # Look at the next block of code for a better understanding of one hot encoding
+    n = j.shape[0]
+    new_array = np.zeros((10, n))
+    index = 0
+    for res in j:
+        new_array[res][index] = 1.0
+        index = index + 1
+    return new_array
+
+data = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+one_hot(data)
 
 
