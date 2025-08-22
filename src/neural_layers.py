bff353be4f507fd76a135e4380a1733bd4a23a2a
hghrv <htchoumkeu@yahoo.fr>
2025-08-22 05:27:57 +0100

    Importing Keras with other libraries and setting extra requirements

---
+++ src/neural_layers.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pickle
+import gzip
+import matplotlib.pyplot as plt
+import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+import h5py
+import sklearn
+import sklearn.datasets
+import scipy
+
+from PIL import Image
+from scipy import ndimage
+
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, BatchNormalization, Activation
+from keras import regularizers
+
+np.random.seed(7)
+
+
+# %matplotlib inline
+
+# 
