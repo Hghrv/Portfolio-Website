2ca32a69196d9bff7eec650961c9ddf11f50c3d0
hghrv <htchoumkeu@yahoo.fr>
2025-09-09 05:34:43 +0100

    Setting final form with enabled submit-button for tests

--- process.php
+++ process.php
@@ -1,9 +1,11 @@
 <?php
 // Using a JavaScript if-statement to trigger the action
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['myButton'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // logic of the action after the setting has been submitted by the user
     echo "Density level submitted successfully!";
     echo "PHP script executed via JavaScript!";
     echo "Get set and ready for the next phase of this presention!";
 }
-?>
+
+// apparently best practise to not do a php closing tag anymore
+// end of file
