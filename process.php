bdbf9b78effd321bd27296d804615444d0f14be2
hghrv <htchoumkeu@yahoo.fr>
2025-09-15 21:05:26 +0100

    Adding corrections to PHP scripts and calls in projects.html

--- process.php
+++ process.php
@@ -2,7 +2,7 @@
 // Using a JavaScript if-statement to trigger the action
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // logic of the action after the setting has been submitted by the user
-    echo "Submitting Layers Density value for backend update.')"    
+    echo "Submitting Layers Density value for backend update.')";  
     document.getElementById("update").addEventListener("click", () => {
         fetch("/src/api.py", { method: "POST" })
           .then(response => response.text())
