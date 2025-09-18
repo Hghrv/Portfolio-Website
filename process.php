8a5c4bd17ff19dd2a3870769a4e36cdf1c565b50
hghrv <htchoumkeu@yahoo.fr>
2025-09-18 08:46:30 +0100

    Implementing Javascript in project.html on browser side in order to fix app buttons before implementing underlying python modules and also PHP for server side applications.

--- process.php
+++ process.php
@@ -1,18 +1,22 @@
 <?php
 // Using a JavaScript if-statement to trigger the action
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // logic of the action after the setting has been submitted by the user
-    echo "Submitting Layers Density value for backend update.')";  
-    document.getElementById("update").addEventListener("click", () => {
-        fetch("/src/api.py", { method: "POST" })
-          .then(response => response.text())
-          .then(data => alert(data))
-          .catch(error => console.error("Error:", error));
-      });
-    echo "Density level submitted successfully!";
-    echo "PHP script executed via JavaScript!";
-    echo "Get set and ready for the next phase of this presention!";    
-}
+echo "Submitting Layers Density value for backend update.";
 
-// Semingly best common practise to not include a php closing tag anymore
-// end of script
+?>
+
+<script type="text/javascript">
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      // logic of the action after the setting has been submitted by the user     
+      document.getElementById("update").addEventListener("submit", () => {
+        fetch("/src/api.py", { method: "POST" }) 
+        .then(response => response.text()) 
+        .then(data => alert(data)) 
+        .catch(error => console.error("Error:", error));
+        alert("Density level submitted successfully!");
+        alert("JavaScript executed via PHP script!");
+        alert("Get set and ready for the next phase of this presention!");    
+      });
+    
+  } 
+</script>
+
